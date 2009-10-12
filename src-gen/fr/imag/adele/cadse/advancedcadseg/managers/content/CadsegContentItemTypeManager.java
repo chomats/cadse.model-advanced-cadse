0663b0075169e3607958849d8e3ff97b94ec63fb
chomats <chomats@imac-de-s-c.local>
2009-10-12 14:17:01 +0200

    add pages add manifest import

---
+++ src-gen/fr/imag/adele/cadse/advancedcadseg/managers/content/CadsegContentItemTypeManager.java
@@ -0,0 +1,60 @@
+package fr.imag.adele.cadse.advancedcadseg.managers.content;
+
+
+import fr.imag.adele.cadse.advancedcadseg.AdvancedCadseGCST;
+import fr.imag.adele.cadse.cadseg.managers.dataModel.ItemTypeManager;
+import fr.imag.adele.cadse.core.Item;
+import fr.imag.adele.cadse.core.ItemType;
+import fr.imag.adele.cadse.core.LinkType;
+
+
+
+/**
+    @generated
+*/
+public class CadsegContentItemTypeManager extends ItemTypeManager {
+
+	/**
+	    @generated
+	*/
+	public CadsegContentItemTypeManager() {
+		super();
+	}
+
+	/**
+		@generated
+	*/
+	@Override
+	public String computeQualifiedName(Item item, String name, Item parent, LinkType lt) {
+		StringBuilder sb = new StringBuilder();
+		try {
+			Object value;
+			Item currentItem;
+			sb.append(parent.getQualifiedName());
+			if (sb.length() != 0) {
+				sb.append(".");
+			}
+			sb.append(name);
+			return sb.toString();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+	/**
+		@generated
+	*/
+	@Override
+	public String getDisplayName(Item item) {
+		try {
+			Object value;
+			return item.getName();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			return "error";
+		}
+	}
+
+}
+
