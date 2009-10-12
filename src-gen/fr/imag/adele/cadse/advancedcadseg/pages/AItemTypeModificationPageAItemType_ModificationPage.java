d9a9ef865d4f1ad1aa3f94091d73744863aed342
chomats <chomats@imac-de-s-c.local>
2009-10-12 14:18:53 +0200

    organize imports

--- src-gen/fr/imag/adele/cadse/advancedcadseg/pages/AItemTypeModificationPageAItemType_ModificationPage.java
+++ src-gen/fr/imag/adele/cadse/advancedcadseg/pages/AItemTypeModificationPageAItemType_ModificationPage.java
@@ -1,30 +1,16 @@
 package fr.imag.adele.cadse.advancedcadseg.pages;
 
-import fede.workspace.model.manager.properties.FieldsCore;
-import fede.workspace.model.manager.properties.IInteractionControllerForList;
 import fede.workspace.model.manager.properties.impl.ic.IC_LinkForBrowser_Combo_List;
 import fede.workspace.model.manager.properties.impl.mc.LinkModelController;
 import fede.workspace.model.manager.properties.impl.mc.StringToBooleanModelControler;
 import fede.workspace.model.manager.properties.impl.ui.DBrowserUI;
 import fede.workspace.model.manager.properties.impl.ui.DCheckBoxUI;
-import fede.workspace.model.manager.properties.impl.ui.DListUI;
 import fede.workspace.model.manager.properties.impl.ui.DTextUI;
-import fr.imag.adele.cadse.cadseg.managers.IC_SuperTypeForBrowser_Combo;
-import fr.imag.adele.cadse.core.CadseException;
 import fr.imag.adele.cadse.core.CadseGCST;
-import fr.imag.adele.cadse.core.IItemNode;
 import fr.imag.adele.cadse.core.Item;
-import fr.imag.adele.cadse.core.ItemType;
-import fr.imag.adele.cadse.core.Link;
-import fr.imag.adele.cadse.core.LinkType;
 import fr.imag.adele.cadse.core.impl.ui.MC_AttributesItem;
 import fr.imag.adele.cadse.core.impl.ui.PageImpl;
 import fr.imag.adele.cadse.core.ui.EPosLabel;
-import fr.imag.adele.cadse.core.ui.IActionPage;
-import fr.imag.adele.cadse.core.ui.IModelController;
-import fr.imag.adele.cadse.core.ui.IPage;
-import fr.imag.adele.cadse.core.ui.PageFactory;
-import fr.imag.adele.cadse.core.ui.UIField;
 
 /**
  @generated
